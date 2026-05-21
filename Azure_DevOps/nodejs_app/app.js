5fab670383b5588136884a8406902351f825d033
aravind1979 <79816564+aravind1979@users.noreply.github.com>
2026-05-21 05:46:00 +0000

    CICD pipleine

---
+++ Azure_DevOps/nodejs_app/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Calculator API is running');
+});
+
+app.get('/calculate/:num1/:op/:num2', (req, res) => {
+    const num1 = parseFloat(req.params.num1);
+    const num2 = parseFloat(req.params.num2);
+    const op = req.params.op;
+    let result;
+
+    if (op === 'add') result = num1 + num2;
+    else if (op === 'sub') result = num1 - num2;
+    else if (op === 'mul') result = num1 * num2;
+    else if (op === 'div') result = num1 / num2;
+
+    res.send({ answer: result });
+});
+
+app.listen(3000, () => console.log('Calculator API running on port 3000'));
---
+++ Azure_DevOps/nodejs_app/index.js
@@ -0,0 +1,1 @@
+"console.log('Hello from Node.js');\n" > index.js
